f43214a10b8cf3f07e0b873fb34c71cb988ae2b6
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-04-27 00:20:49 +0300

    login page start

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -26,7 +26,7 @@ const Orders = () => {
               />
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-centergap-3 mt-2 text-base text-gray-700">
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
                   <p className="text-lg">
                     {currency}
                     {item.price}
@@ -34,10 +34,18 @@ const Orders = () => {
                   <p>Quantity: 1</p>
                   <p>Size: M</p>
                 </div>
-                <p className="mt-2 ">
+                <p className="mt-2">
                   Date <span className="text-gray-400">25 July, 2024</span>
                 </p>
               </div>
+            </div>
+
+            <div className="md:w-1/2  flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                <p className="sm:text-sm text-base ">Ready to ship</p>
+              </div>
+              <button className="border px-4 py-2 text-sm font-medium rounded-sm cursor-pointer">Track Order</button>
             </div>
           </div>
         ))}
