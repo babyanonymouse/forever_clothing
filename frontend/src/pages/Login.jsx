a164ff353648cc15e895e1e72ba47e7a2291dda3
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-04-27 00:46:52 +0300

    login and signup states

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,12 +9,19 @@ const Login = () => {
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
       </div>
-      <input
-        type="text"
-        className="w-full px-3 py-2 border border-gray-800"
-        placeholder="Name"
-        required
-      />
+
+      {/* initialise login */}
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+
       <input
         type="email"
         className="w-full px-3 py-2 border border-gray-800"
@@ -27,6 +34,30 @@ const Login = () => {
         placeholder="Password"
         required
       />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forgot your password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Signup")}
+            className="cursor-pointer"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login here
+          </p>
+        )}
+      </div>
+      <button
+        className=" cursor-pointer bg-black text-white font-light px-8 py-2
+      mt-4"
+      >
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
     </form>
   );
 };
