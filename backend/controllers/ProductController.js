88ea39312ae95987ae06ec37d27988f68c72a449
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-05-02 15:27:19 +0300

    list product from DB

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -56,7 +56,15 @@ const addProduct = async (req, res) => {
 };
 
 // list all products function
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // remove product function
 const removeProduct = async (req, res) => {};
