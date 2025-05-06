b211747509a9726d24e1570624a3ac5ec45e8020
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-05-06 13:12:19 +0300

    routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
@@ -10,6 +14,13 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-800 text-base">
+            <Routes>
+              <Route path="/add" element={<Add />}></Route>
+              <Route path="/list" element={<List />}></Route>
+              <Route path="/orders" element={<Orders />}></Route>
+            </Routes>
+          </div>
         </div>
       </>
     </div>
