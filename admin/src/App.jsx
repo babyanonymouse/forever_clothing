4a01d7ecb1f98dba98187c5396bb9e4e2bd59390
Anonymouse Baby <babyanonymouse2@gmail.com>
2025-05-06 17:22:55 +0300

    admin authentication

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+// toastify
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr />
-        <div className="flex w-full">
-          <Sidebar />
-          <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-800 text-base">
-            <Routes>
-              <Route path="/add" element={<Add />}></Route>
-              <Route path="/list" element={<List />}></Route>
-              <Route path="/orders" element={<Orders />}></Route>
-            </Routes>
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-800 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />}></Route>
+                <Route path="/list" element={<List />}></Route>
+                <Route path="/orders" element={<Orders />}></Route>
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
